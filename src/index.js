13f5ae4dcae16a4b6c40b887fbb1261b2eabbfd5
lrh <a991776189@163.com>
2022-11-27 22:39:59 +0800

    feat(react): reactdom render

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from './react';
-import ReactDOM from 'react-dom';
+import ReactDOM from './react-dom';
+
+//普通组件
 // let ele = <h1 className='title' style={{color: 'red'}}>Hello</h1>
 
-let ele2 = React.creatElement("h1", {
-  className: 'title',
-  style: {
-    color: 'red'
-  }
-}, 666)
+// let ele2 = React.creatElement("h1", {
+//   className: 'title',
+//   style: {
+//     color: 'red'
+//   }
+// }, 666)
 
+//函数式组件
+function FunctionComponent(props) {
+  return React.creatElement("h1", {style: {color: 'red'}}, 'hello react');
+}
+
+// let ele = <FunctionComponent name="100"></FunctionComponent>
+
+let ele2 = React.creatElement(FunctionComponent, {
+  name: "100"
+})
 //vdom => real dom
 ReactDOM.render(
   ele2,
--- src/react-dom.js
+++ src/react-dom.js
@@ -1,27 +1,55 @@
 import {REACT_TEXT} from './stants';
 
 function render(vdom, container) {
+  mount(vdom, container);
+}
+
+function mount(vdom, container) {
   let dom = createDom(vdom);
   container.appendChild(dom);
 }
 
 function createDom(vdom) {
-  let {type, props} = vdom;
+  let {type, props, content} = vdom;
   let dom;
   if(type == REACT_TEXT) {
-    dom = document.createTextNode(type);
+    dom = document.createTextNode(content);
+  }else if(typeof type == 'function') {
+    return mountFunctionComponent(vdom);
   }else {
     dom = document.createElement(type);
   }
-  props && updateProps(dom, {}, props);
+
+  if(props) {
+    updateProps(dom, {}, props);
+    let children = props.children;
+    children && changeChildren(children, dom)
+  }
 
   return dom;
 }
 
+//处理函数式组件
+function mountFunctionComponent(vdom) {
+  const {type, props} = vdom;
+  let functionVdom = type(props);
+  return createDom(functionVdom);
+}
+
+//处理children
+function changeChildren(children, dom) {
+  //1. 有一个children
+  if (typeof children == 'object' && children.type) {
+    mount(children, dom)
+  }else if(Array.isArray(children)) { //多个儿子
+    children.forEach(item => mount(item, dom))
+  }
+}
+//更新属性
 function updateProps(dom, oldProps, newProps) {
   if(newProps) {
     for (var key in newProps) {
-      if(ket == 'children') {
+      if(key == 'children') {
         continue;
       }else if(key == 'style') {
         let styleObj = newProps[key];
